f3a0ae72f3100cc4d2568a95e298183480f988cf
Rosana Rezende <rezende_rosana@hotmail.com>
2020-12-04 12:40:09 -0300

    refatoração

--- 01-FRONTEND/PROJETO.1-TO.DO.LIST/tasks.js
+++ 01-FRONTEND/PROJETO.1-TO.DO.LIST/tasks.js
@@ -1,5 +1,6 @@
 let result = document.querySelector("#result");
 let tasksArray = [];
+let taskClicked;
 
 const tasksInLocalStorage = JSON.parse(localStorage.getItem("tasks"));
 if (tasksInLocalStorage) {
@@ -7,41 +8,6 @@ if (tasksInLocalStorage) {
   for (let task of tasksArray) {
     createTaskAtHTML(task);
   }
-}
-
-const checks = document.querySelectorAll("input[type=checkbox]");
-for (let check of checks) {
-  check.addEventListener("click", (event) => {
-    for (let task of tasksArray) {
-      if (task.id === Number(event.target.id)) {
-        task.checked = !task.checked;
-      }
-      if (task.checked) {
-        event.target.setAttribute("checked", "checked");
-      } else {
-        event.target.removeAttribute("checked");
-      }
-    }
-    addTasksAtLocalStorage();
-  });
-}
-
-let tempIdToDelete;
-let tempDivToDelete;
-const iconsDelete = document.querySelectorAll("#deletar-tarefa");
-for (let iconDelete of iconsDelete) {
-  iconDelete.removeAttribute("data-dismiss");
-  iconDelete.addEventListener("click", (event) => {
-    setAttributes(
-      event.target,
-      "data-toggle",
-      "modal",
-      "data-target",
-      "#modal2"
-    );
-    tempIdToDelete = event.target.parentElement.children[0].children[0].id;
-    tempDivToDelete = event.target.parentElement;
-  });
 }
 
 function addTasksAtLocalStorage() {
@@ -55,75 +21,137 @@ function setAttributes(elem) {
 }
 
 function createTaskAtHTML(task) {
-  // criar checkbox
+  // ========== criar checkbox ==========
   const checkbox = document.createElement("input");
-  if (task.checked) {
-    setAttributes(checkbox, "type", "checkbox", "checked", "checked");
-  } else {
-    checkbox.setAttribute("type", "checkbox");
-  }
+  checkbox.type = "checkbox";
   checkbox.className = "check";
   checkbox.id = task?.id;
+  if (task.checked) {
+    checkbox.setAttribute("checked", "checked");
+  }
 
-  // cria label da tarefa
-  const text = document.createTextNode(task?.text);
+  checkbox.addEventListener("click", () => {
+    task.checked = !task.checked;
+    if (task.checked) {
+      checkbox.setAttribute("checked", "checked");
+    } else {
+      checkbox.removeAttribute("checked");
+    }
+    addTasksAtLocalStorage();
+  });
+
+  // ========== cria label da tarefa ==========
   const label = document.createElement("label");
-  label.appendChild(text);
+  const text = document.createTextNode(task?.text);
   label.htmlFor = task?.id;
+  label.appendChild(text);
 
-  // colocar checkbox e label em uma div
+  // ========== colocar checkbox e label em uma div ==========
   const div = document.createElement("div");
-  div.setAttribute("style", "display: flex;");
   div.appendChild(checkbox);
   div.appendChild(label);
   div.className = "label-checkbox";
+  div.setAttribute("style", "display: flex;");
 
-  // cria botão de deletar
+  // ========== cria botão de deletar ==========
   const button = document.createElement("button");
   const closeIcon = document.createTextNode("\u00D7");
-  button.className = "close";
-  button.id = "deletar-tarefa";
   button.appendChild(closeIcon);
+  button.className = "close2";
+  button.id = task?.id;
   setAttributes(button, "data-tt", "tooltip", "title", "Deletar tarefa!");
+  
+  button.addEventListener("click", () => {
+    setAttributes(button, "data-toggle", "modal", "data-target", "#modal2");
+    taskClicked = button.parentElement.parentElement;
+  });
 
-  // cria div maior, que receberá a div anterior e botão de deletar
+  // ========== cria botão de editar ==========
+  const button2 = document.createElement("button");
+  const editIcon = document.createTextNode("\u270f");
+  button2.appendChild(editIcon);
+  button2.className = "edit";
+  button2.id = task?.id;
+  setAttributes(button2, "data-tt", "tooltip", "title", "Editar tarefa!");
+  
+  button2.addEventListener("click", () => {
+    const newInputTask = document.querySelector("#new-input-text");
+    for (let task of tasksArray) {
+      if (task.id === Number(button2.id)) {
+        newInputTask.value = task.text;
+      }
+    }    
+    taskClicked = button2.parentElement.parentElement;
+    setAttributes(button2, "data-toggle", "modal", "data-target", "#modal3");
+  });
+
+  // ========== colocar botões de editar e deletar uma div ==========
+  const div2 = document.createElement("div");
+  div2.appendChild(button2);
+  div2.appendChild(button);
+  div2.className = "edit-delete";
+  div2.setAttribute("style", "display: flex;");
+
+  // ========== cria div maior, que receberá as divs anteriores ==========
   const newTaskElement = document.createElement("div");
-  newTaskElement.className = "nova-tarefa";
   newTaskElement.appendChild(div);
-  newTaskElement.appendChild(button);
+  newTaskElement.appendChild(div2);
+  newTaskElement.className = "new-task";
+  newTaskElement.id = task?.id;
 
+  // ========== colocar no HTML ==========
   result.appendChild(newTaskElement);
 }
 
-function clickToCreateTask() {
-  const btnAdd = document.querySelector("#btn-add");
-  const valueInputTask = document.querySelector("#input-tarefa").value;
-  const alternativeText = document.querySelector("#com-texto");
-
-  btnAdd.removeAttribute("data-dismiss");
-
-  if (valueInputTask === "") {
+function checkInput(idInput, idAlternativeText, idButtonConfirm) {
+  const valueInput = document.querySelector(`#${idInput}`).value;
+  const alternativeText = document.querySelector(`#${idAlternativeText}`);
+  const btnConfirm = document.querySelector(`#${idButtonConfirm}`);
+  btnConfirm.removeAttribute("data-dismiss");
+  if (valueInput === "") {
     alternativeText.setAttribute("style", "display: block; color: red");
   } else {
+    document.querySelector(`#${idInput}`).value = "";
+    alternativeText.setAttribute("style", "display: none;");
+    btnConfirm.setAttribute("data-dismiss", "modal");
+  }
+  return valueInput;
+}
+
+function clickToCreateTask() {
+  const inputValue = checkInput("input-tarefa", "com-texto", "btn-add");
+  if (inputValue) {
     let task = {
       id: new Date().getTime(),
       checked: false,
-      text: valueInputTask,
+      text: inputValue,
     };
     tasksArray.push(task);
     createTaskAtHTML(task);
     addTasksAtLocalStorage();
+  }
+}
 
-    alternativeText.setAttribute("style", "display: none;");
-    btnAdd.setAttribute("data-dismiss", "modal");
-    document.querySelector("#input-tarefa").value = "";
+function confirmEdit() {
+  const inputValue = checkInput("new-input-text", "com-texto2", "btn-change");
+  if (inputValue) {
+    for (let task of tasksArray) {
+      if (task.id === Number(taskClicked.id)) {
+        task.text = inputValue;
+      }
+    }
+    result.innerHTML = "";
+    for (let task of tasksArray) {
+      createTaskAtHTML(task);
+    }
+    addTasksAtLocalStorage();
   }
 }
 
 function confirmDelete() {
   const btnDelete = document.querySelector("#btn-delete");
   btnDelete.setAttribute("data-dismiss", "modal");
-  tasksArray = tasksArray.filter((task) => task.id !== Number(tempIdToDelete));
-  result.removeChild(tempDivToDelete);
+  tasksArray = tasksArray.filter((task) => task.id !== Number(taskClicked.id));
+  result.removeChild(taskClicked);
   addTasksAtLocalStorage();
 }
