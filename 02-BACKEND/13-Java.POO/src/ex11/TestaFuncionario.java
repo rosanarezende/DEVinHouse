b9ee70aa937e436a144bc1b3a2d8ccedfe03e709
Rosana Rezende <rezende_rosana@hotmail.com>
2021-03-22 08:19:52 -0300

    [SP13] Exercício 11 - corrigido

--- 02-BACKEND/13-Java.POO/src/ex11/TestaFuncionario.java
+++ 02-BACKEND/13-Java.POO/src/ex11/TestaFuncionario.java
@@ -5,8 +5,8 @@ public class TestaFuncionario {
         Funcionario fulano = new Funcionario("Fulano", "de Tal", 1500);
         Funcionario beltrano = new Funcionario("Beltrano", "de Tal", 2000);
 
-        System.out.println(fulano.getSalario());
-        System.out.println(beltrano.getSalario());
+        System.out.println(fulano.getSalario() * 12);
+        System.out.println(beltrano.getSalario() * 12);
 
         Double fulanoSalarioMais10PorCento = fulano.getSalario() * 1.1;
         Double betranoSalarioMais10PorCento = beltrano.getSalario() * 1.1;
@@ -14,7 +14,7 @@ public class TestaFuncionario {
         fulano.setSalario(fulanoSalarioMais10PorCento);
         beltrano.setSalario(betranoSalarioMais10PorCento);
 
-        System.out.println(fulano.getSalario());
-        System.out.println(beltrano.getSalario());
+        System.out.println(fulano.getSalario() * 12);
+        System.out.println(beltrano.getSalario() * 12);
     }
 }
